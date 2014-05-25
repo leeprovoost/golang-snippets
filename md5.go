4337952a8b81afdaeb304f7c5c821d753227a395
Lee Provoost <lee.provoost@gmail.com>
2014-05-25 22:02:21 +0100

    added md5 script

---
+++ md5.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(GetMD5Hash("Hello World"))
+}
+
+// Create an MD5 hash based on a string
+// See: http://stackoverflow.com/questions/2377881/how-to-get-a-md5-hash-from-a-string-in-golang
+func GetMD5Hash(text string) string {
+	hashText := md5.New()
+	hashText.Write([]byte(text))
+	return hex.EncodeToString(hashText.Sum(nil))
+}
